43b4ad133f0fc0d5a85f05e6156f6ad41927aaf5
Adrian Marti <adrianmarti06@gmail.com>
2021-06-07 20:49:15 +0200

    Change to typeclasses + bundled structures hybrid
    This works surprisingly well. Though I have not repaired all the
    proofs.
    
    I think I am going to refactor this to use unification hints.

--- CategoryTheory/Category.lean
+++ CategoryTheory/Category.lean
@@ -6,7 +6,7 @@ structure HomStruct where
   hom : obj → obj → Type v
 
 
-class Category (C : HomStruct) where
+class IsCategory (C : HomStruct) where
   id' : (c : C.obj) → C.hom c c
   comp : {c d e : C.obj} → C.hom c d → C.hom d e → C.hom c e
   id_comp : {c d : C.obj} → (f : C.hom c d) → comp (id' c) f = f
@@ -15,27 +15,48 @@ class Category (C : HomStruct) where
       → (h : C.hom c d) → comp (comp f g) h = comp f (comp g h)
 
 open HomStruct
-open Category
+open IsCategory
 
 notation:80 lhs " ∘ " rhs:81  => comp rhs lhs
 
 attribute [simp] id_comp comp_id assoc
 
+-- Note that this is suspiciously close to the fully bundled approach
+-- + unification hints. I should really try that - especially since
+-- we then don't have a `IsCategory`. I doubt this is useful for anything
+-- since I don't see `HomStruct` being used for anything if it is not
+-- a category.
 
-structure BundledCategory where
+-- Note that while we don't know how scalable unification hints will
+-- be, they may still be more performant overall because when we pass
+-- a category to a function explicitly, we don't need to infer
+-- anything(e.g. when considering a type of functors).
+
+-- Automatic inference will only be used when using the `∘` notation.
+
+-- It seems like some of the yoneda proofs need to be repaired to
+-- work with bundled categories, but I think I will refactor this to
+-- use unification hints for `∘` anyways.
+structure Category where
   base : HomStruct
-  inst : Category base
+  inst : IsCategory base
 
-instance (C: BundledCategory) : Category C.base := C.inst
+-- We also want all `HomStruct` fields
+def Category.obj (C: Category) := C.base.obj
+
+def Category.hom (C: Category) := C.base.hom
+
+instance (C: Category) : IsCategory C.base := C.inst
 
 section Set
 
 universe u
 
-abbrev SmallCategory := BundledCategory.{u,u}
-abbrev LargeCategory := BundledCategory.{u+1,u}
+abbrev SmallCategory := Category.{u,u}
+abbrev LargeCategory := Category.{u+1,u}
 
 
+-- Note that we don't even to construct this by providing an instance
 def Set : LargeCategory := {
   base := {
     obj := Type u,
@@ -66,26 +87,25 @@ def HomStruct.opposite (C: HomStruct) : HomStruct := {
 
 notation:1030 arg "ᵒᵖ"  => HomStruct.opposite arg
 
-instance Category.opposite (C: HomStruct) [Category C]: Category Cᵒᵖ := {
-  id' := fun c => id' (C := C) c
-  comp := fun f g => comp (C := C) g f
-  id_comp := by
-    intros
-    simp
-  comp_id := by
-    intros
-    simp
-  assoc := by
-    intros
-    simp
+def Category.opposite (C: Category): Category := {
+  base := HomStruct.opposite C.base,
+  inst := {
+    id' := fun c => id' (C := C.base) c
+    comp := fun f g => comp (C := C.base) g f
+    id_comp := by
+      intros
+      simp
+    comp_id := by
+      intros
+      simp
+    assoc := by
+      intros
+      simp
+  }
 }
 
--- Note that this doesn't allow for the usual trick where we
--- replace `C` by `Cᵒᵖ` in a lemma and then reduce `Cᵒᵖᵒᵖ` to `C`
--- since this is *an equality of `HomStruct`s* and that means that
--- we can't construct a motive where the `HomStruct` `C` has the
--- `[Category C]` instance.
--- We would need bundled structures to actually use this trick.
+notation:1030 arg "ᵒᵖ"  => Category.opposite arg
+
 theorem opop (C: HomStruct) : Cᵒᵖᵒᵖ = C  := by
   revert C
   intro { obj := obj, hom := hom}
@@ -94,16 +114,16 @@ theorem opop (C: HomStruct) : Cᵒᵖᵒᵖ = C  := by
 attribute [simp] opop
 
 
-def inverses (C: HomStruct) [Category C] {c d: C.obj} (f: C.hom c d) (g: C.hom d c) := g ∘ f = id' c ∧ f ∘ g = id' d
+def inverses (C: Category) {c d: C.obj} (f: C.hom c d) (g: C.hom d c) := g ∘ f = id' c ∧ f ∘ g = id' d
 
-theorem inverse_unique (C: HomStruct) [Category C] {c d: C.obj} (f: C.hom c d) (g h: C.hom d c) : inverses C f g ∧ inverses C f h → g = h := by
+theorem inverse_unique (C: Category) {c d: C.obj} (f: C.hom c d) (g h: C.base.hom d c) : inverses C f g ∧ inverses C f h → g = h := by
   intro ⟨⟨_,p⟩,⟨q,_⟩⟩
   have r : (h ∘ f) ∘ g = h := by
     rw [<- assoc, p, id_comp]
 
   rw [<- r, q, comp_id]
 
-def isomorphism (C: HomStruct) [Category C] {c d: C.obj} (f: C.hom c d) :=
+def isomorphism (C: Category) {c d: C.obj} (f: C.hom c d) :=
   exists (g: C.hom d c), inverses C f g
 
 namespace Function
@@ -130,7 +150,7 @@ def isomorphism (f: A → B) :=
 
 end Function
 
-structure Functor (C D: HomStruct) [Category C] [Category D] where
+structure Functor (C D: Category) where
   obj : C.obj → D.obj
   map : {c d : C.obj} → C.hom c d → D.hom (obj c) (obj d)
   map_id : {c : C.obj} → map (id' c) = id' (obj c)
@@ -138,7 +158,7 @@ structure Functor (C D: HomStruct) [Category C] [Category D] where
 
 attribute [simp] Functor.map_id Functor.map_comp
 
-theorem Functor.ext {C D : HomStruct} [Category C] [Category D]: (F G : Functor C D) → (p: F.obj = G.obj) →
+theorem Functor.ext {C D : Category}: (F G : Functor C D) → (p: F.obj = G.obj) →
   (q: (fun c d (f: C.hom c d) => F.map f)
     ≅ (fun c d (f: C.hom c d) => G.map f))
   → F = G
@@ -151,7 +171,7 @@ theorem Functor.ext {C D : HomStruct} [Category C] [Category D]: (F G : Functor 
   exact ⟨p,q⟩
 
 
-def fId (C: HomStruct) [Category C] : Functor C C := {
+def fId (C: Category) : Functor C C := {
   obj := id
   map := id
   map_id := by
@@ -162,7 +182,7 @@ def fId (C: HomStruct) [Category C] : Functor C C := {
     rfl
 }
 
-def fComp {C D E: HomStruct} [Category C] [Category D] [Category E] (F: Functor C D) (G: Functor D E) : Functor C E := {
+def fComp {C D E: Category} (F: Functor C D) (G: Functor D E) : Functor C E := {
   obj := Function.comp G.obj F.obj
   map := fun f => G.map (F.map f)
   map_id := by
@@ -174,12 +194,12 @@ def fComp {C D E: HomStruct} [Category C] [Category D] [Category E] (F: Functor 
 }
 
 def Cat : HomStruct.{max (u + 1) (v + 1), max u v} := {
-  obj := BundledCategory.{u,v},
-  hom := fun C D => Functor C.base D.base
+  obj := Category.{u,v},
+  hom := fun C D => Functor C D
 }
 
-instance : Category Cat where
-  id' := fun {C} => fId C.base
+instance : IsCategory Cat where
+  id' := fun {C} => fId C
   comp := fComp
   id_comp := by
     intros
@@ -209,13 +229,12 @@ instance : Category Cat where
     exact HEq.rfl
 
 
-def fully_faithful {C: HomStruct} [Category C] {D: HomStruct} [Category D] (F: Functor C D) :=
+def fully_faithful {C D: Category} (F: Functor C D) :=
   forall c d : C.obj, Function.isomorphism (F.map (c := c) (d := d))
 
 section NatTrans
 
-variable {C : HomStruct} [Category C]
-variable {D : HomStruct} [Category D]
+variable {C D: Category}
 
 structure NatTrans (F G : Functor C D) :=
   app : (c : C.obj) → D.hom (F.obj c) (G.obj c)
@@ -232,9 +251,15 @@ theorem NatTrans.ext {F G : Functor C D} : (η μ : NatTrans F G) → (p: η.app
   exact p
 
 
-def FunctorCat (C: HomStruct) [Category C] (D: HomStruct) [Category D]: HomStruct := {
-  obj := Functor C D
-  hom := NatTrans
+def vComp {F G H : Functor C D} (η : NatTrans F G) (μ : NatTrans G H) : NatTrans F H := {
+  app := fun c => (app μ c) ∘ (app η c),
+  naturality := by
+    intro c d f
+    simp
+    rw [<- naturality μ f]
+    rw [<- IsCategory.assoc]
+    rw [naturality η f]
+    rw [IsCategory.assoc]
 }
 
 def idTrans (F : Functor C D) : NatTrans F F := {
@@ -244,32 +269,27 @@ def idTrans (F : Functor C D) : NatTrans F F := {
     simp
 }
 
-def vComp {F G H : Functor C D} (η : NatTrans F G) (μ : NatTrans G H) : NatTrans F H := {
-  app := fun c => (app μ c) ∘ (app η c),
-  naturality := by
-    intro c d f
-    simp
-    rw [<- naturality μ f]
-    rw [<- Category.assoc]
-    rw [naturality η f]
-    rw [Category.assoc]
+def FunctorCat (C D: Category): Category := {
+  base := {
+    obj := Functor C D
+    hom := NatTrans
+  }
+  inst := {
+    id'     := idTrans,
+    comp    := vComp,
+    id_comp := by
+      intros
+      apply NatTrans.ext
+      simp [id', idTrans, comp, vComp]
+    comp_id := by
+      intros
+      apply NatTrans.ext
+      simp [id', idTrans, comp, vComp]
+    assoc := by
+      intros
+      apply NatTrans.ext
+      simp [comp, vComp]
 }
-
-instance : Category (FunctorCat C D) := {
-  id'     := idTrans,
-  comp    := vComp,
-  id_comp := by
-    intros
-    apply NatTrans.ext
-    simp [id', idTrans, comp, vComp]
-  comp_id := by
-    intros
-    apply NatTrans.ext
-    simp [id', idTrans, comp, vComp]
-  assoc := by
-    intros
-    apply NatTrans.ext
-    simp [comp, vComp]
 }
 
 end NatTrans
@@ -279,7 +299,7 @@ section Yoneda
 universes u v
 
 
-variable {C : HomStruct.{u,v}} [Category C]
+variable {C : Category.{u,v}}
 
 
 def yObj (c: C.obj) : (Functor Cᵒᵖ Set.{v}) := {
@@ -287,11 +307,24 @@ def yObj (c: C.obj) : (Functor Cᵒᵖ Set.{v}) := {
     -- f gets sent to precomposition with f
     map := fun f g => g ∘ f
     map_id := by
-      intros
-      simp [id']
+      intro d
+      simp
+      funext f
+      -- While it seems like the lhs can be simplified, this is not true
+      -- since `id'` is in `Cᵒᵖ` and not in `C`
+      -- I need a better solution for this...
+      have p: id' (C := C.base) d = id' (C := Cᵒᵖ.base) d  := by rfl
+      rw [<- p]
+      simp
+      rfl
+
     map_comp := by
-      intros
-      simp [comp]
+      intros x y z f g
+      simp
+      funext h
+      have p: comp (C := C.base) g f = comp (C := Cᵒᵖ.base) f g  := by rfl
+      rw [<- p]
+      simp [comp, Set]
   }
 
 
@@ -299,7 +332,7 @@ def yMap {c d: C.obj} (f: C.hom c d) : NatTrans (yObj c) (yObj d) := {
   app := fun c g => f ∘ g
   naturality := by
     intros
-    simp [yObj, comp]
+    simp [yObj, comp, Set]
 }
 
 def y : Functor C (FunctorCat Cᵒᵖ Set.{v}) := {
@@ -308,7 +341,8 @@ def y : Functor C (FunctorCat Cᵒᵖ Set.{v}) := {
   map_id := by
     intros
     apply NatTrans.ext
-    simp [yMap, id', idTrans]
+    funext _ _
+    simp [yObj, yMap]
   map_comp := by
     intros
     apply NatTrans.ext
